e7b254bdd9508a8b6ee7a3265cd9c028f043dd13
ludovic <ludovic.angenard@orange.fr>
2020-12-18 11:54:37 +0100

    the game.js is on a draft state. But every modules are required.

--- src/game.js
+++ src/game.js
@@ -1,19 +1,23 @@
 const http = require('http');
 //const fs = require('fs');
 const grid = require('./Grid');
+const player = require('./Player');
 const html = require('./gridHtml.js');
 const io = require('socket.io')(http);
-
+const { JSDOM } = require( "jsdom" );
+const { window } = new JSDOM( "" );
+const $ = require( "jquery" )( window );
 // création du serveur
 const server = http.createServer((req, res) =>{
     //traitement de la transaction HTTP
-    console.log(grid);
     let test = new grid();
     var contenu = html.buildPageGrid(test.html);
-    test.resetGrid();
+    var joueur = new player(1,'red');
 
+    test.resetGrid();
     res.end(contenu);
 });
+
 
 // Ecouteur sur le port 9000
 const port = 9000;
